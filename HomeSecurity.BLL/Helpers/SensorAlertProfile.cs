f0864156600d2260868383bf3135d6b0fb210f19
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 13:45:52 +0300

    Add SensorAlertProfile for AutoMapper configurations
    Introduces the `SensorAlertProfile` class in the
    `HomeSecurity.BLL.Helpers` namespace. This class
    inherits from `Profile` and defines two reversible
    mapping configurations: one between
    `SensorAlertCreateDto` and `SensorAlert`, and
    another between `SensorAlert` and `SensorAlertDto`.

---
+++ HomeSecurity.BLL/Helpers/SensorAlertProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using HomeSecurity.BLL.DTOs.SensorAlerts;
+using HomeSecurity.DAL.Entities;
+
+namespace HomeSecurity.BLL.Helpers;
+
+public class SensorAlertProfile : Profile
+{
+    public SensorAlertProfile()
+    {
+        CreateMap<SensorAlertCreateDto, SensorAlert>()
+            .ReverseMap();
+
+        CreateMap<SensorAlert, SensorAlertDto>()
+            .ReverseMap();
+    }
+}
