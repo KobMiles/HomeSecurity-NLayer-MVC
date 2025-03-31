497fe0f21d3808ffb10047b3c0f25ab6c7fa917a
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:37:12 +0300

    Add SensorAlertsWithSensorSpec for querying alerts
    This commit introduces the `SensorAlertsWithSensorSpec` class in the `HomeSecurity.DAL.Entities.Specifications.SensorAlerts` namespace. The class extends `Specification<SensorAlert>` and includes a constructor that configures a query to include related `Sensor` entities and their `Location` entities.

---
+++ HomeSecurity.DAL/Entities/Specifications/SensorAlerts/SensorAlertsWithSensorSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+
+namespace HomeSecurity.DAL.Entities.Specifications.SensorAlerts;
+
+public class SensorAlertsWithSensorSpec : Specification<SensorAlert>
+{
+    public SensorAlertsWithSensorSpec()
+    {
+        Query.Include(sa => sa.Sensor)
+            .ThenInclude(s => s.Location);
+    }
+}
