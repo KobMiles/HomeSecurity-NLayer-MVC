df553c6e566c74835132ccce332a4010fe514d42
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:27:48 +0300

    Add RedirectAuthenticatedAttribute for user redirection
    This commit introduces a new class `RedirectAuthenticatedAttribute` that extends `ActionFilterAttribute`. It includes properties for `RedirectAction` and `RedirectController`, defaulting to "Index" and "Home". The overridden `OnActionExecuting` method checks user authentication and redirects authenticated users to the specified action and controller.

---
+++ HomeSecurity-NLayer-MVC/Filters/RedirectAuthenticatedAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeSecurity_NLayer_MVC.Filters;
+
+public class RedirectAuthenticatedAttribute : ActionFilterAttribute
+{
+    public string RedirectAction { get; set; } = "Index";
+    public string RedirectController { get; set; } = "Home";
+
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var user = context.HttpContext.User;
+        if (user.Identity?.IsAuthenticated == true)
+        {
+            context.Result = new RedirectToActionResult(RedirectAction, RedirectController, null);
+        }
+    }
+}
