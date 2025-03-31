360f194f9abc31460acb688cb5fcbf9e0de5c0cb
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:37:21 +0300

    Add UserByIdWithTicketsSpec for user ID queries
    Introduces a new specification class `UserByIdWithTicketsSpec` in the `HomeSecurity.DAL.Entities.Specifications.Users` namespace. This class allows querying users by their ID, with a constructor that accepts a `userId` parameter to filter the results accordingly.

---
+++ HomeSecurity.DAL/Entities/Specifications/Users/UserByIdWithTicketsSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace HomeSecurity.DAL.Entities.Specifications.Users;
+
+public class UserByIdWithTicketsSpec : Specification<User>
+{
+    public UserByIdWithTicketsSpec(string userId)
+    {
+        Query.Where(u => u.Id == userId);
+    }
+}
