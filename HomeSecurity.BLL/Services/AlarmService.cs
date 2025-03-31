875f67e36358bb18b0ea4482542263005d065804
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:36:06 +0300

    Add AlarmService with sensor alert retrieval functionality
    Introduces the `AlarmService` class in the `HomeSecurity.BLL.Services` namespace, implementing the `IAlarmService` interface. Adds the `GetSensorAlertsAsync` method to retrieve and sort sensor alerts by date, sensor type, or room location, utilizing AutoMapper for DTO mapping. Includes necessary using directives for improved code organization.

---
+++ HomeSecurity.BLL/Services/AlarmService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using HomeSecurity.BLL.DTOs.SensorAlerts;
+using HomeSecurity.BLL.Interfaces.Services;
+using HomeSecurity.DAL.Entities.Enums;
+using HomeSecurity.DAL.Entities.Specifications.SensorAlerts;
+using HomeSecurity.DAL.Interfaces.Repositories;
+
+namespace HomeSecurity.BLL.Services;
+
+public class AlarmService(IUnitOfWork unitOfWork, IMapper mapper) : IAlarmService
+{
+    public async Task<IEnumerable<SensorAlertDto>> GetSensorAlertsAsync(string sortOrder = "date_desc")
+    {
+        var spec = new SensorAlertsWithSensorSpec();
+        var alerts = await unitOfWork.SensorAlerts.ListAsync(spec);
+
+        switch (sortOrder)
+        {
+            case "date_asc":
+                alerts = alerts.OrderBy(a => a.Timestamp).ToList();
+                break;
+            case "date_desc":
+                alerts = alerts.OrderByDescending(a => a.Timestamp).ToList();
+                break;
+            case "sensorType":
+                alerts = alerts.OrderBy(a => a.Sensor.SensorType).ToList();
+                break;
+            case "room":
+                alerts = alerts.OrderBy(a => a.Sensor.Location.Name).ToList();
+                break;
+            default:
+                break;
+        }
+
+        return mapper.Map<IEnumerable<SensorAlertDto>>(alerts);
+    }
+}
