b64867933317a5d5c35fb818bd0384e8d03776c2
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:36:33 +0300

    Add UserLoginDtoValidator for login data validation
    Introduces a new `UserLoginDtoValidator` class that extends
    `AbstractValidator<UserLoginDto>`. This class validates the
    `Email` and `Password` fields, ensuring the email is not
    empty and in a valid format, while the password is not
    empty and has a minimum length of 6 characters.
    Error messages are provided in Ukrainian.

---
+++ HomeSecurity.BLL/Validators/UserLoginDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using HomeSecurity.BLL.DTOs.Users;
+
+namespace HomeSecurity.BLL.Validators;
+
+public class UserLoginDtoValidator : AbstractValidator<UserLoginDto>
+{
+    public UserLoginDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email обов'язковий")
+            .EmailAddress().WithMessage("Не вірний email");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Пароль обов'язковий")
+            .MinimumLength(6).WithMessage("Пароль має бути більшим ніж 6 символів"); ;
+    }
+}
