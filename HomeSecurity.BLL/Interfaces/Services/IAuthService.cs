5505743b9b368eb8b2228bf00e76d0d0cf4c40c5
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:35:53 +0300

    Add authentication methods to IAuthService interface
    Introduced using directives for user DTOs and ASP.NET Core Identity. Added methods for user registration, login, and logout in the IAuthService interface.

---
+++ HomeSecurity.BLL/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using HomeSecurity.BLL.DTOs.Users;
+using Microsoft.AspNetCore.Identity;
+using IdentitySignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace HomeSecurity.BLL.Interfaces.Services;
+
+public interface IAuthService
+{
+    Task<IdentityResult> RegisterAsync(UserRegisterDto registerDto);
+
+    Task<IdentitySignInResult> LoginAsync(UserLoginDto loginDto);
+
+    Task LogoutAsync();
+}
