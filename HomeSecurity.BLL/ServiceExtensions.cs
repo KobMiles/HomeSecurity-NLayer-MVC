f2cc7ff76383009e5c8e55369bb634385dc40a3d
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:37:06 +0300

    Add FluentValidation and new service registrations
    Updated `ServiceExtensions.cs` to include FluentValidation using directives and added service registrations for `IUserService`, `IAuthService`, and `IAlarmService`. Introduced a new method, `AddFluentValidation`, to configure FluentValidation features such as auto-validation and client-side adapters.

--- HomeSecurity.BLL/ServiceExtensions.cs
+++ HomeSecurity.BLL/ServiceExtensions.cs
@@ -1,5 +1,8 @@
-﻿using HomeSecurity.BLL.Interfaces.Services;
+﻿using FluentValidation.AspNetCore;
+using FluentValidation;
+using HomeSecurity.BLL.Interfaces.Services;
 using HomeSecurity.BLL.Services;
+using HomeSecurity.BLL.Validators;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HomeSecurity.BLL;
@@ -8,12 +11,24 @@ public static class ServiceExtensions
     public static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
     {
         services.AddScoped<ISensorService, SensorService>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IAlarmService, AlarmService>();
+
         return services;
     }
 
     public static IServiceCollection AddAutoMapper(this IServiceCollection services)
     {
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        return services;
+    }   
+    public static IServiceCollection AddFluentValidation(this IServiceCollection services)
+    {
+        services.AddFluentValidationAutoValidation();
+        services.AddFluentValidationClientsideAdapters();
+        services.AddValidatorsFromAssemblyContaining<UserRegisterDtoValidator>();
 
         return services;
     }
