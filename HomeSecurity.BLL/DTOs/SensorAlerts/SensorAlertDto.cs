d4ed46c37de042bd371aa21b0352e1edfc5aca8a
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:31:22 +0300

    Enhance SensorAlertDto with new properties
    Updated the `SensorAlertDto` class to include `SensorName`, `SensorType`, and `LocationName` properties, all initialized to an empty string. The `SensorId` property remains unchanged but has been repositioned within the class.

--- HomeSecurity.BLL/DTOs/SensorAlerts/SensorAlertDto.cs
+++ HomeSecurity.BLL/DTOs/SensorAlerts/SensorAlertDto.cs
@@ -3,7 +3,10 @@
 public class SensorAlertDto
 {
     public int Id { get; set; }
-    public int SensorId { get; set; }
     public DateTime Timestamp { get; set; }
     public string? Message { get; set; }
+    public int SensorId { get; set; }
+    public string SensorName { get; set; } = string.Empty;
+    public string SensorType { get; set; } = string.Empty;
+    public string LocationName { get; set; } = string.Empty;
 }
