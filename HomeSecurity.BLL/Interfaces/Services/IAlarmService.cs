74d531fe3617e6669d9d4603c5b98ef5cc7d1ee0
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:35:47 +0300

    Add IAlarmService interface for sensor alerts
    Introduces a new using directive for SensorAlerts DTOs and defines the IAlarmService interface. This interface includes the GetSensorAlertsAsync method, which returns a task of enumerable SensorAlertDto objects, allowing for sorting by date.

---
+++ HomeSecurity.BLL/Interfaces/Services/IAlarmService.cs
@@ -0,0 +1,8 @@
+﻿using HomeSecurity.BLL.DTOs.SensorAlerts;
+
+namespace HomeSecurity.BLL.Interfaces.Services;
+
+public interface IAlarmService
+{
+    Task<IEnumerable<SensorAlertDto>> GetSensorAlertsAsync(string sortOrder = "date_desc");
+}
