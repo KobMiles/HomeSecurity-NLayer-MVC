23ebb5001db46381eacd45a6d42ec60f71962e04
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:28:09 +0300

    Add ProfileViewModel with user properties
    Introduces the `ProfileViewModel` class in the
    `HomeSecurity_NLayer_MVC.ViewModels` namespace.
    It contains two properties: `CurrentUser` of type
    `UserDto` and `AllUsers`, an enumerable collection
    of `UserDto`. A using directive for
    `HomeSecurity.BLL.DTOs.Users` is also added.

---
+++ HomeSecurity-NLayer-MVC/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,9 @@
+﻿using HomeSecurity.BLL.DTOs.Users;
+
+namespace HomeSecurity_NLayer_MVC.ViewModels;
+
+public class ProfileViewModel
+{
+    public UserDto CurrentUser { get; set; } = null!;
+    public IEnumerable<UserDto> AllUsers { get; set; } = new List<UserDto>();
+}
