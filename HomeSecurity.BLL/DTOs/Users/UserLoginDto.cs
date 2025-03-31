1b4ccfb1e0f0c9cb3969e565f5e2f855d137791c
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:34:31 +0300

    Add UserLoginDto class for user authentication
    Introduced a new `UserLoginDto` class in the `HomeSecurity.BLL.DTOs.Users` namespace. This class contains three properties: `Email`, `Password`, and `RememberMe`, with `Email` and `Password` marked as required.

---
+++ HomeSecurity.BLL/DTOs/Users/UserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HomeSecurity.BLL.DTOs.Users;
+
+public class UserLoginDto
+{
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public bool RememberMe { get; set; }
+}
