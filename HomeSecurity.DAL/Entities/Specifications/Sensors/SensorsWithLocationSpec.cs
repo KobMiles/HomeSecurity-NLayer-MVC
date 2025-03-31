e778e5dc541c42c63f6174b12ecad3dceb82c1d9
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 13:46:59 +0300

    Add SensorsWithLocationSpec for sensor location queries
    Introduced a new specification class `SensorsWithLocationSpec` in the `HomeSecurity.DAL.Entities.Specifications.Sensors` namespace. This class extends `Specification<Sensor>` and includes a query to include the `Location` property of the `Sensor` entity.

---
+++ HomeSecurity.DAL/Entities/Specifications/Sensors/SensorsWithLocationSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+
+namespace HomeSecurity.DAL.Entities.Specifications.Sensors;
+
+public class SensorsWithLocationSpec : Specification<Sensor>
+{
+    public SensorsWithLocationSpec()
+    {
+        Query
+            .Include(s => s.Location);
+    }
+}
