a4d10b93038e20116950fb91c8c519cfbbc68b3b
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 13:45:59 +0300

    Add SensorProfile for mapping Sensor to SensorDto
    Introduces the `SensorProfile` class in the `HomeSecurity.BLL.Helpers` namespace. This class uses AutoMapper to create a mapping between the `Sensor` entity and the `SensorDto` data transfer object. The mapping includes populating the `LocationName` property from the `Location.Name` and supports reverse mapping for both directions.

---
+++ HomeSecurity.BLL/Helpers/SensorProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HomeSecurity.BLL.DTOs.Sensors;
+using HomeSecurity.DAL.Entities;
+
+namespace HomeSecurity.BLL.Helpers;
+
+public class SensorProfile : Profile
+{
+    public SensorProfile()
+    {
+        CreateMap<Sensor, SensorDto>()
+            .ForMember(dest => dest.LocationName, 
+                opt => opt.MapFrom(src => src.Location.Name))
+            .ReverseMap();
+    }
+}
