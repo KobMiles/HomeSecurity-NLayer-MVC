62592b28bf7604206537d46923bb59187256236e
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:36:25 +0300

    Add UserService with user retrieval methods
    Introduces a new `UserService` class in the `HomeSecurity.BLL.Services` namespace that implements the `IUserService` interface. This class includes two asynchronous methods: `GetUserDetailsAsync` for retrieving user details by user ID and `GetAllUsersAsync` for fetching all users. The methods utilize a specification pattern and AutoMapper for DTO mapping. Necessary namespaces are also imported.

---
+++ HomeSecurity.BLL/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using HomeSecurity.BLL.DTOs.Users;
+using HomeSecurity.BLL.Interfaces.Services;
+using HomeSecurity.DAL.Entities.Specifications.Users;
+using HomeSecurity.DAL.Interfaces.Repositories;
+
+namespace HomeSecurity.BLL.Services;
+
+public class UserService(IUnitOfWork unitOfWork, IMapper mapper) : IUserService
+{
+    public async Task<UserDto> GetUserDetailsAsync(string userId)
+    {
+        var spec = new UserByIdWithTicketsSpec(userId);
+        var user = await unitOfWork.Users.FirstOrDefaultAsync(spec)
+                   ?? throw new KeyNotFoundException($"User with id: {userId} not found.");
+
+        var userDetailsDto = mapper.Map<UserDto>(user);
+
+        return userDetailsDto;
+    }
+
+    public async Task<IEnumerable<UserDto>> GetAllUsersAsync()
+    {
+        var users = await unitOfWork.Users.ListAsync();
+        return mapper.Map<IEnumerable<UserDto>>(users);
+    }
+}
