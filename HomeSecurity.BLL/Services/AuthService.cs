2916a8ac1fc85dc8cac9d78d4bc9df689c87a386
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:36:12 +0300

    Add AuthService for user management and authentication
    Introduces the `AuthService` class in the `HomeSecurity.BLL.Services` namespace, implementing the `IAuthService` interface. This class provides methods for user registration, login, and logout using ASP.NET Core Identity's `UserManager` and `SignInManager`. The `RegisterAsync` method handles user creation, while `LoginAsync` authenticates users via email and password. The `LogoutAsync` method manages user sign-out. Necessary using directives for AutoMapper and Identity are also included.

---
+++ HomeSecurity.BLL/Services/AuthService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using HomeSecurity.BLL.DTOs.Users;
+using HomeSecurity.BLL.Interfaces.Services;
+using HomeSecurity.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using IdentitySignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace HomeSecurity.BLL.Services;
+
+public class AuthService(
+    UserManager<User> userManager,
+    SignInManager<User> signInManager,
+    IMapper mapper) : IAuthService
+{
+    public async Task<IdentityResult> RegisterAsync(UserRegisterDto registerDto)
+    {
+        var user = mapper.Map<User>(registerDto);
+
+        var result = await userManager.CreateAsync(user, registerDto.Password);
+        if (!result.Succeeded)
+            return result;
+
+        return result;
+    }
+
+    public async Task<IdentitySignInResult> LoginAsync(UserLoginDto loginDto)
+    {
+        var user = await userManager.FindByEmailAsync(loginDto.Email);
+        if (user == null)
+        {
+            return IdentitySignInResult.Failed;
+        }
+
+        var result = await signInManager.PasswordSignInAsync(
+            user,
+            loginDto.Password,
+            loginDto.RememberMe,
+            lockoutOnFailure: false
+        );
+
+        return result;
+    }
+
+    public async Task LogoutAsync()
+    {
+        await signInManager.SignOutAsync();
+    }
+}
