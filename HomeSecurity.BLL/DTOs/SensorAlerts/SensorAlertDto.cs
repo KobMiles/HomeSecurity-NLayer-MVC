ce5cc9d6dff1891be2aca0248821ecddbb89e7ac
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 13:45:30 +0300

    Add SensorAlertDto class for sensor alert data
    Introduced a new `SensorAlertDto` class in the
    `HomeSecurity.BLL.DTOs.SensorAlerts` namespace.
    This class contains properties for `Id`, `SensorId`,
    `Timestamp`, and a nullable `Message` to represent
    sensor alert information.

---
+++ HomeSecurity.BLL/DTOs/SensorAlerts/SensorAlertDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeSecurity.BLL.DTOs.SensorAlerts;
+
+public class SensorAlertDto
+{
+    public int Id { get; set; }
+    public int SensorId { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string? Message { get; set; }
+}
