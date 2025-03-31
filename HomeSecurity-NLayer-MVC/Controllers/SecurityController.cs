d6c6ddc9652908eb6603f386676cddd12124016f
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:27:33 +0300

    Add SecurityController with sensor data retrieval
    Introduces a new `SecurityController` class in the
    `HomeSecurity_NLayer_MVC.Controllers` namespace. The class
    includes authorization requirements and features two action
    methods: `Index` for returning a view and `GetSensors`
    for asynchronously fetching updated sensor data from
    `ISensorService`, returning it as JSON.

---
+++ HomeSecurity-NLayer-MVC/Controllers/SecurityController.cs
@@ -0,0 +1,21 @@
+﻿using HomeSecurity.BLL.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeSecurity_NLayer_MVC.Controllers;
+[Authorize]
+public class SecurityController(ISensorService sensorService) : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetSensors()
+    {
+        var updatedSensors = await sensorService.UpdateSensorsRandomlyAsync();
+        return Json(updatedSensors);
+    }
+}
