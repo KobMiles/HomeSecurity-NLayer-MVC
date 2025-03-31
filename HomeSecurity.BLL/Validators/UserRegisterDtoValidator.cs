6e0af067f536737423557214979aac791250efcb
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 19:36:41 +0300

    Add UserRegisterDtoValidator for registration validation
    Implemented validation rules for user registration data, including checks for email, first name, last name, password, and confirm password, with appropriate error messages.

---
+++ HomeSecurity.BLL/Validators/UserRegisterDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using HomeSecurity.BLL.DTOs.Users;
+
+namespace HomeSecurity.BLL.Validators;
+
+public class UserRegisterDtoValidator : AbstractValidator<UserRegisterDto>
+{
+    public UserRegisterDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email обов'язковий")
+            .EmailAddress().WithMessage("Не вірний email");
+
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("Ім'я є обов'язковим.")
+            .MaximumLength(100).WithMessage("Ім'я має бути менше 100 символів.");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Прізвище є обов'язковим.")
+            .MaximumLength(100).WithMessage("Прізвище має бути менше 100 символів.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Пароль обов'язковий")
+            .MinimumLength(6).WithMessage("Пароль має бути більшим ніж 6 символів");
+
+        RuleFor(x => x.ConfirmPassword)
+            .Equal(x => x.Password).WithMessage("Паролі не співпадають");
+    }
+}
