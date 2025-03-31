249ab4d3872c38a0915ac80794dac35664328913
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 18:54:54 +0300

    Add AlarmsController for managing sensor alerts
    Introduces the `AlarmsController` class in the
    `HomeSecurity_NLayer_MVC.Controllers` namespace.
    Includes a constructor that accepts an `IAlarmService`
    and an `Index` method to asynchronously retrieve
    sensor alerts based on a sorting order, updating
    the `ViewData` with the current sort order.

---
+++ HomeSecurity-NLayer-MVC/Controllers/AlarmsController.cs
@@ -0,0 +1,14 @@
+﻿using HomeSecurity.BLL.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeSecurity_NLayer_MVC.Controllers;
+
+public class AlarmsController(IAlarmService sensorAlertService) : Controller
+{
+    public async Task<IActionResult> Index(string sortOrder = "date_desc")
+    {
+        var alerts = await sensorAlertService.GetSensorAlertsAsync(sortOrder);
+        ViewData["CurrentSort"] = sortOrder;
+        return View(alerts);
+    }
+}
