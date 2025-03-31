6d6d24ffa0ff643fe4861b55713083d18a50f020
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-31 13:46:37 +0300

    Add ServiceExtensions for business logic and AutoMapper
    Introduces a new static class `ServiceExtensions` in the
    `HomeSecurity.BLL` namespace. This class provides two
    extension methods for `IServiceCollection`:
    - `AddBusinessLogicServices` to register `ISensorService`
      with its implementation `SensorService` as a scoped service.
    - `AddAutoMapper` to configure AutoMapper to scan all
      assemblies in the current application domain for mapping
      profiles. Additional using directives are also included.

---
+++ HomeSecurity.BLL/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using HomeSecurity.BLL.Interfaces.Services;
+using HomeSecurity.BLL.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeSecurity.BLL;
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+    {
+        services.AddScoped<ISensorService, SensorService>();
+        return services;
+    }
+
+    public static IServiceCollection AddAutoMapper(this IServiceCollection services)
+    {
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        return services;
+    }
+}
