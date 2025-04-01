92db4be882ad6492503011f283810936771b0c6e
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-04-01 10:12:58 +0300

    Add AlarmStatus class to HomeSecurity.DAL.Entities
    Introduced a new `AlarmStatus` class implementing the `IEntity` interface. The class includes three properties: `Id`, `IsActive` (defaulting to true), and `LastUpdated` (initialized to the current date and time).

---
+++ HomeSecurity.DAL/Entities/AlarmStatus.cs
@@ -0,0 +1,10 @@
+﻿using HomeSecurity.DAL.Interfaces;
+
+namespace HomeSecurity.DAL.Entities;
+
+public class AlarmStatus : IEntity
+{
+    public int Id { get; set; }
+    public bool IsActive { get; set; } = true;
+    public DateTime LastUpdated { get; set; } = DateTime.Now;
+}
