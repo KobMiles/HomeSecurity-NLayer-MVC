de1e4c7328bd7f1988a85a69ca6f8e87c9b40ed4
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-04-01 10:11:44 +0300

    Add AlarmStatusConfiguration for EF Core setup
    Introduces the `AlarmStatusConfiguration` class implementing
    `IEntityTypeConfiguration<AlarmStatus>`. Configures the
    `AlarmStatus` entity with a primary key and marks
    `IsActive` and `LastUpdated` properties as required.

---
+++ HomeSecurity.DAL/Configurations/AlarmStatusConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using HomeSecurity.DAL.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeSecurity.DAL.Configurations;
+
+public class AlarmStatusConfiguration : IEntityTypeConfiguration<AlarmStatus>
+{
+    public void Configure(EntityTypeBuilder<AlarmStatus> builder)
+    {
+        builder
+            .HasKey(a => a.Id);
+
+        builder
+            .Property(a => a.IsActive)
+            .IsRequired();
+
+        builder
+            .Property(a => a.LastUpdated)
+            .IsRequired();
+    }
+}
