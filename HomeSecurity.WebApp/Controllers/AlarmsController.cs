b4ea5a1a39f469e5d08f108905ab7ade6b9e7a7c
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-04-01 17:42:24 +0300

    Add authorization to AlarmsController
    This commit introduces the `Authorize` attribute to the `AlarmsController` class, ensuring that only authorized users can access its actions. It also includes the necessary `Microsoft.AspNetCore.Authorization` namespace and corrects the constructor syntax for dependency injection.

--- HomeSecurity.WebApp/Controllers/AlarmsController.cs
+++ HomeSecurity.WebApp/Controllers/AlarmsController.cs
@@ -1,8 +1,10 @@
 ﻿using HomeSecurity.BLL.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HomeSecurity_NLayer_MVC.Controllers;
 
+[Authorize]
 public class AlarmsController(IAlarmService sensorAlertService) : Controller
 {
     public async Task<IActionResult> Index(string sortOrder = "date_desc")
