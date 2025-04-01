39dc5d238ccf31fd8847d70a2dcf11f7bb13b0eb
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-04-01 10:13:36 +0300

    Add InitialCreate2 migration for database setup
    This commit introduces the `InitialCreate2` migration for the `HomeSecurity` application. It creates essential database tables including `AlarmStatuses`, `Locations`, `Users`, and `Sensors`, along with various identity-related tables. The migration also seeds initial data for `AlarmStatuses` and `Locations`, and establishes foreign key relationships between the `Sensors` and `Locations` tables, ensuring the database schema is correctly configured to support application functionality.

--- HomeSecurity.DAL/Migrations/20250331190419_InitialCreate2.Designer.cs
+++ HomeSecurity.DAL/Migrations/20250331190419_InitialCreate2.Designer.cs
@@ -4,6 +4,7 @@ using HomeSecurity.DAL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomeSecurity.DAL.Migrations
 {
     [DbContext(typeof(HomeSecurityDbContext))]
-    partial class HomeSecurityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250331190419_InitialCreate2")]
+    partial class InitialCreate2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
