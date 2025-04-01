0eafa16236ff4c7ec16a819cd045145fc932b14e
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-04-01 10:11:26 +0300

    Refactor AlarmService and add alarm status methods
    - Updated imports to include HomeSecurity.DAL.Entities.
    - Changed sorting logic for alerts to use new syntax.
    - Simplified switch case for "room" sorting by removing redundant break.
    - Added GetAlarmStatusAsync and ToggleAlarmStatusAsync methods to manage alarm status.

--- HomeSecurity.BLL/Services/AlarmService.cs
+++ HomeSecurity.BLL/Services/AlarmService.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
 using HomeSecurity.BLL.DTOs.SensorAlerts;
 using HomeSecurity.BLL.Interfaces.Services;
-using HomeSecurity.DAL.Entities.Enums;
+using HomeSecurity.DAL.Entities;
 using HomeSecurity.DAL.Entities.Specifications.SensorAlerts;
 using HomeSecurity.DAL.Interfaces.Repositories;
 
@@ -17,21 +17,42 @@ public class AlarmService(IUnitOfWork unitOfWork, IMapper mapper) : IAlarmServic
         switch (sortOrder)
         {
             case "date_asc":
-                alerts = alerts.OrderBy(a => a.Timestamp).ToList();
+                alerts = [.. alerts.OrderBy(a => a.Timestamp)];
                 break;
             case "date_desc":
-                alerts = alerts.OrderByDescending(a => a.Timestamp).ToList();
+                alerts = [.. alerts.OrderByDescending(a => a.Timestamp)];
                 break;
             case "sensorType":
-                alerts = alerts.OrderBy(a => a.Sensor.SensorType).ToList();
+                alerts = [.. alerts.OrderBy(a => a.Sensor.SensorType)];
                 break;
             case "room":
-                alerts = alerts.OrderBy(a => a.Sensor.Location.Name).ToList();
-                break;
-            default:
+                alerts = [.. alerts.OrderBy(a => a.Sensor.Location.Name)];
                 break;
         }
 
         return mapper.Map<IEnumerable<SensorAlertDto>>(alerts);
+    }
+
+    public async Task<bool> GetAlarmStatusAsync()
+    {
+        var status = await unitOfWork.AlarmStatuses.GetByIdAsync(1);
+        return status?.IsActive ?? true;
+    }
+
+    public async Task ToggleAlarmStatusAsync()
+    {
+        var status = await unitOfWork.AlarmStatuses.GetByIdAsync(1);
+        if (status == null)
+        {
+            status = new AlarmStatus { IsActive = true };
+            await unitOfWork.AlarmStatuses.AddAsync(status);
+        }
+        else
+        {
+            status.IsActive = !status.IsActive;
+            status.LastUpdated = DateTime.Now;
+            await unitOfWork.AlarmStatuses.UpdateAsync(status);
+        }
+        await unitOfWork.SaveAsync();
     }
 }
