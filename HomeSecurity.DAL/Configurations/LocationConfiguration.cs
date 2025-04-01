3295ad3dd111f4fe9e170a3edd389f45a60a26a7
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-04-01 10:11:51 +0300

    Improve code readability in LocationConfiguration
    Refactor the `Configure` method in the `LocationConfiguration` class by adding line breaks and indentation to method calls. This change enhances the clarity of the code structure while preserving its functionality.

--- HomeSecurity.DAL/Configurations/LocationConfiguration.cs
+++ HomeSecurity.DAL/Configurations/LocationConfiguration.cs
@@ -8,13 +8,16 @@ public class LocationConfiguration : IEntityTypeConfiguration<Location>
 {
     public void Configure(EntityTypeBuilder<Location> builder)
     {
-        builder.HasKey(l => l.Id);
+        builder
+            .HasKey(l => l.Id);
 
-        builder.Property(l => l.Name)
+        builder
+            .Property(l => l.Name)
             .IsRequired()
             .HasMaxLength(100);
 
-        builder.HasMany(l => l.Sensors)
+        builder
+            .HasMany(l => l.Sensors)
             .WithOne(s => s.Location)
             .HasForeignKey(s => s.LocationId);
     }
